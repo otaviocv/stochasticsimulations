5d092dc1307de18296f882e6057f97597acebeb9
otaviocv <otavio@deluqui.com.br>
2016-11-04 00:57:26 -0200

    Add features to piprog.py and lingevin.py
    Fix some bugs on piprog.py
    Add correlation calculus and graphs to lingevin.py

--- lingevin.py
+++ lingevin.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def main():
     tau =  0.01
     n = int(input("n:"))
-    gamma = [1,2,3,4]
+    gamma = [1,5,10,20]
     v = [0,0,0,0]
     velocities = [[0],[0],[0],[0]]
     csi = normal(0,1)
@@ -24,18 +24,42 @@ def main():
         velocities[1].append(v[1])
         velocities[2].append(v[2])
         velocities[3].append(v[3])
-        print(v)
+        # print(v)
 
-    print(velocities)
-    plt.plot(velocities[0])
-    plt.plot(velocities[1])
-    plt.plot(velocities[2])
-    plt.plot(velocities[3])
+    correlacao = [[],[],[],[]]
+    for i in range(0, n//2):
+        means = [0,0,0,0]
+        for j in range(0, n-i):
+            means[0] += velocities[0][j]*velocities[0][j+i]/(n-i)
+            means[1] += velocities[1][j]*velocities[1][j+i]/(n-i)
+            means[2] += velocities[2][j]*velocities[2][j+i]/(n-i)
+            means[3] += velocities[3][j]*velocities[3][j+i]/(n-i)
+        correlacao[0].append(means[0])
+        correlacao[1].append(means[1])
+        correlacao[2].append(means[2])
+        correlacao[3].append(means[3])
+
+    # print(velocities)
+    p1 = plt.plot(velocities[0])
+    p2 = plt.plot(velocities[1])
+    p3 = plt.plot(velocities[2])
+    p4 = plt.plot(velocities[3])
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), (str(gamma[0]), str(gamma[1]),
+        str(gamma[2]), str(gamma[3])))
     plt.show()
-    plt.hist(velocities[0], alpha=0.5)
-    plt.hist(velocities[1], alpha=0.5)
-    plt.hist(velocities[2], alpha=0.5)
     plt.hist(velocities[3], alpha=0.5)
+    plt.hist(velocities[2], alpha=0.5)
+    plt.hist(velocities[1], alpha=0.5)
+    plt.hist(velocities[0], alpha=0.5)
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), (str(gamma[0]), str(gamma[1]),
+        str(gamma[2]), str(gamma[3])))
+    plt.show()
+    plt.plot(correlacao[0])
+    plt.plot(correlacao[1])
+    plt.plot(correlacao[2])
+    plt.plot(correlacao[3])
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), (str(gamma[0]), str(gamma[1]),
+        str(gamma[2]), str(gamma[3])))
     plt.show()
 
 main()
--- pi.py
+++ pi.py
@@ -7,16 +7,12 @@ def main():
     Seed = int(input("seed:"))
     seed(Seed)
     M = 0
-    points = []
     for i in range(1,N+1):
         x = random()
         y = random()
         # print(str(x) + " " + str(y))
         if (x*x + y*y <= 1):
             M += 1
-        points.append(4*M/i)
-
-
 
     pi = 4*M/N
     print("pi: " + str(pi))
