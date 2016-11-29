6c4a2ed226a8dcaf36db228d173271ee6b2c24fb
otaviocv <otavio@deluqui.com.br>
2016-11-29 16:07:21 -0200

    Add mean calc and dashed lines to plot in urnas1

--- urnas1.py
+++ urnas1.py
@@ -1,5 +1,6 @@
 from random import randint
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     # As bolas na urna A são denotadas com 0 e as bolas na urna B são denotadas
@@ -13,7 +14,7 @@ def main():
     for corridas in range(0,totalCorridas):
         n = 100
         bolas = [0]*totalBolas
-        print("Corrida %d" % corridas)
+        # print("Corrida %d" % corridas)
         media[0] += n/totalCorridas
         for t in range(1,totalPassos):
             troca = randint(0,totalBolas - 1)
@@ -24,6 +25,14 @@ def main():
                 n -= 1
             media[t] += n/totalCorridas
 
+    M = np.mean(media)
+    M2 = np.mean(media[300:])
+    print("Media: %f" % M)
+    print("Media 200:1000: %f" % M2)
+    t = np.arange(0, totalPassos, 1)
+    plt.plot(t, t-t+M, 'r--')
+    t = np.arange(0, totalPassos, 1)
+    plt.plot(t, t-t+M2, 'g--')
     plt.plot(media)
     plt.axis([0,totalPassos,0,totalBolas])
     plt.show()
